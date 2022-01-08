d44ac66d638350899e6c6f9866b6b02ffa5b8bfe
Marcos Henrique Pereira da Silva <marcos.silva@compasso.com.br>
2022-01-07 21:25:43 -0300

    backend

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite maximo de '{MAX}'."
+
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'"
